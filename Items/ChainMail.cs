e4f4136dad48a077622e8a667a7d7ae5638fba65
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-04-10 13:46:01 +0900

    [class] Add Equipments
    - 롱소드 클래스 추가
    - 체인메일 클래스 추가

---
+++ Items/ChainMail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGA_OOPConsoleProject.Items
+{
+    public class ChainMail : Armor
+    {
+        public ChainMail() 
+        {
+            name = "체인메일";
+            description = "튼튼하지만 많이 무겁다. 군데군데 녹슬었다";
+            defence = 15;
+            buyGold = 250;
+            sellGold = 150;
+        }
+        public ChainMail(Vector2 position): base(position)
+        {
+            name = "체인메일";
+            description = "튼튼하지만 많이 무겁다. 군데군데 녹슬었다";
+            defence = 15;
+            buyGold = 250;
+            sellGold = 150;
+        }
+    }
+}
--- Items/LongSword.cs
+++ Items/LongSword.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace KGA_OOPConsoleProject.Items
 {
-    public class ShortKnife : Weapon
+    public class LongSword:Weapon
     {
-
-        public ShortKnife(Vector2 position) :base(position)
+        public LongSword()
         {
-            name = "단검";
-            description = "짧고 뭉툭한 단검";
-            power = 5;
-            buyGold = 150;
-            sellGold = 75;
+            name = "롱소드";
+            description = "칼날이 무뎠지만 무게감이 좋은 검";
+            power = 10;
+            buyGold = 300;
+            sellGold = 150;
         }
-        public ShortKnife()
+        public LongSword(Vector2 position) : base(position)
         {
             name = "단검";
             description = "짧고 뭉툭한 단검";
